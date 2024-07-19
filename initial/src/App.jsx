97e1a45e5eeaee4a350839df2d45d6aba30e05e1
msohaaib <msohaib968505@gmail.com>
2024-07-19 16:05:45 +0500

    updated

--- initial/src/App.jsx
+++ initial/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Nav from './Components/Nav'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <Nav/>
     </>
   )
 }
---
+++ initial/src/Components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='text-5xl'>
+      hello world
+    </div>
+  )
+}
