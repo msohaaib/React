2cd841010c25081d61008ffc8cc821e1548cfa4c
msohaaib <msohaib968505@gmail.com>
2024-07-22 22:19:19 +0500

    updated

--- TailwindProps/src/Components/Card.jsx
+++ TailwindProps/src/Components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({userName, btnText}) {
   return (
-    <div className="w-[300px] rounded-md border">
+    <div className="w-[300px] rounded-md border my-4">
   <img
     src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
     alt="Laptop"
