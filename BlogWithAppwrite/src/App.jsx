1597641ae9b009eb4da87f29621e3208b236e1fd
msohaaib <msohaib968505@gmail.com>
2024-07-30 16:17:41 +0500

    updated

---
+++ BlogWithAppwrite/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1>React App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ BlogWithAppwrite/src/appwrite/auth.js
@@ -0,0 +1,50 @@
+import conf from "../conf/conf";
+import {Client, Account, ID} from "appwrite"
+
+export class AuthService {
+    Client = new Client()
+    accoount;
+
+    constructor(){
+        this.Client
+                .setEndpoint(conf.appWriteUrl)
+                .setProject(conf.appWriteProjectId);
+        this.accoount = new Account(this.Client)
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.accoount.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({email, password})
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async login({email, password}){
+        try {
+            return await this.accoount.createEmailSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.accoount.get()
+        } catch (error) {
+            throw error
+        }
+        return null
+    }
+}
+
+
+const authService = new AuthService()
+
+export default AuthService
